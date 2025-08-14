5c965e8a4e4a5632686d6f339f648d9cc32ef066
Wieedze <maxime.moodz@gmail.com>
2025-08-14 15:24:40 +0200

    adress fixed and plugin loaded with any agent

--- SofIA/agent/src/index.ts
+++ SofIA/agent/src/index.ts
@@ -1,21 +1,20 @@
 import { logger, type IAgentRuntime, type Project, type ProjectAgent } from '@elizaos/core';
 import starterPlugin from './plugin.ts';
-import { gaianetPlugin } from './plugins/gaianet/src/index.ts'
+import { gaianetPlugin } from '../../plugins/gaianet/src/index.ts'
 import { character } from './character.ts';
 import { ProjectStarterTestSuite } from './__tests__/e2e/project-starter.e2e';
 
 const initCharacter = ({ runtime }: { runtime: IAgentRuntime }) => {
   runtime.registerPlugin(gaianetPlugin);
   logger.info('Initializing character');
-  logger.info({ name: character.name }, 'Name:');
+  logger.info({ name: runtime.character?.name || 'Unknown' }, 'Character loaded:');
 };
 
 export const projectAgent: ProjectAgent = {
   character,
   init: async (runtime: IAgentRuntime) => await initCharacter({ runtime }),
-
   plugins: [gaianetPlugin],
-  tests: [ProjectStarterTestSuite], // Export tests from ProjectAgent
+  tests: [ProjectStarterTestSuite],
 };
 
 const project: Project = {
