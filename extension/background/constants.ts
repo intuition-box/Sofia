6f593d28f64cf9ec42be2c20962a661cfcf706af
Wieedze <maxime.moodz@gmail.com>
2025-09-11 06:46:49 +0200

    before refacto echoes tab

--- extension/background/constants.ts
+++ extension/background/constants.ts
@@ -28,7 +28,7 @@ export const BOOKMARKAGENT_IDS = {
 // --- Constants ---
 export const USER_NAME = "User";
 
-// Source identifier for this Next.js application
+
 export const CHAT_SOURCE = "API";
 
 
