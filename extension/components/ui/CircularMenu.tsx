7327b47ec7922d82443de7037376cf87d6ee4590
Wieedze <maxime.moodz@gmail.com>
2025-10-01 10:33:39 +0200

    svg display with blur

--- extension/components/ui/CircularMenu.tsx
+++ extension/components/ui/CircularMenu.tsx
@@ -17,18 +17,13 @@ const CircularMenu = ({ isVisible, onItemClick }: CircularMenuProps) => {
       title: 'Pulse Tab',
       position: { left: 'calc(50% - 140px)', top: 'calc(50% - 80px)' },
       icon: (
-        <svg width="60" height="60" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
-          <circle className="outer-circle" cx="50" cy="50" r="40" stroke="black" strokeWidth="2.8" fill="none" strokeDasharray="68.1 4.4 12.6 4.4" />
-          <circle className="middle-circle" cx="50" cy="50" r="32" stroke="black" strokeWidth="4" fill="none" />
-          <circle className="inner-circle" cx="50" cy="50" r="26" fill="white" />
-          <g transform="translate(50, 50) translate(-12, -12) scale(1)">
-            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-              <path d="M17 3h2a2 2 0 0 1 2 2v2"/>
-              <path d="M21 17v2a2 2 0 0 1-2 2h-2"/>
-              <path d="M3 7V5a2 2 0 0 1 2-2h2"/>
-              <path d="M7 21H5a2 2 0 0 1-2-2v-2"/>
-              <path d="M7.828 13.07A3 3 0 0 1 12 8.764a3 3 0 0 1 4.172 4.306l-3.447 3.62a1 1 0 0 1-1.449 0z"/>
-            </svg>
+        <svg width="160px" height="160px" viewBox="0 0 80 80" xmlns="http://www.w3.org/2000/svg" style={{display: 'block', maxWidth: 'none', maxHeight: 'none'}}>
+          <g transform="translate(40, 40) scale(4) translate(-12, -12)">
+            <path d="M17 3h2a2 2 0 0 1 2 2v2" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" fill="none"/>
+            <path d="M21 17v2a2 2 0 0 1-2 2h-2" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" fill="none"/>
+            <path d="M3 7V5a2 2 0 0 1 2-2h2" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" fill="none"/>
+            <path d="M7 21H5a2 2 0 0 1-2-2v-2" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" fill="none"/>
+            <path d="M7.828 13.07A3 3 0 0 1 12 8.764a3 3 0 0 1 4.172 4.306l-3.447 3.62a1 1 0 0 1-1.449 0z" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" fill="none"/>
           </g>
         </svg>
       ),
@@ -42,16 +37,11 @@ const CircularMenu = ({ isVisible, onItemClick }: CircularMenuProps) => {
       title: 'Import Data',
       position: { left: 'calc(50% + 140px)', top: 'calc(50% - 80px)' },
       icon: (
-        <svg width="60" height="60" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
-          <circle className="outer-circle" cx="50" cy="50" r="40" strokeWidth="2.8" fill="none" stroke="black" strokeDasharray="68.1 4.4 12.6 4.4" />
-          <circle className="middle-circle" cx="50" cy="50" r="32" strokeWidth="4" fill="none" stroke="black" />
-          <circle className="inner-circle" cx="50" cy="50" r="26" fill="white" />
-          <g transform="translate(50, 50) translate(-12, -12) scale(1)">
-            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-              <path d="M12 3v12"/>
-              <path d="m8 11 4 4 4-4"/>
-              <path d="M8 5H4a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V7a2 2 0 0 0-2-2h-4"/>
-            </svg>
+        <svg width="160px" height="160px" viewBox="0 0 80 80" xmlns="http://www.w3.org/2000/svg" style={{display: 'block', maxWidth: 'none', maxHeight: 'none'}}>
+          <g transform="translate(40, 40) scale(4) translate(-12, -12)">
+            <path d="M12 3v12" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" fill="none"/>
+            <path d="m8 11 4 4 4-4" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" fill="none"/>
+            <path d="M8 5H4a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V7a2 2 0 0 0-2-2h-4" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" fill="none"/>
           </g>
         </svg>
       ),
@@ -65,16 +55,11 @@ const CircularMenu = ({ isVisible, onItemClick }: CircularMenuProps) => {
       title: 'Find Similar',
       position: { left: 'calc(50% + 0px)', top: 'calc(50% + 140px)' },
       icon: (
-        <svg width="60" height="60" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
-          <circle className="outer-circle" cx="50" cy="50" r="40" strokeWidth="2.8" fill="none" stroke="black" strokeDasharray="68.1 4.4 12.6 4.4" />
-          <circle className="middle-circle" cx="50" cy="50" r="32" strokeWidth="4" fill="none" stroke="black" />
-          <circle className="inner-circle" cx="50" cy="50" r="26" fill="white" />
-          <g transform="translate(50, 50) translate(-14, -14) scale(1)">
-            <svg width="28" height="28" viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <circle cx="14" cy="14" r="5.05" stroke="black" strokeWidth="1.9"/>
-              <circle cx="14" cy="14" r="9.05" stroke="black" strokeOpacity="0.5" strokeWidth="1.9"/>
-              <circle cx="14" cy="14" r="13.05" stroke="black" strokeOpacity="0.2" strokeWidth="1.9"/>
-            </svg>
+        <svg width="160px" height="160px" viewBox="0 0 80 80" xmlns="http://www.w3.org/2000/svg" style={{display: 'block', maxWidth: 'none', maxHeight: 'none'}}>
+          <g transform="translate(40, 40) scale(4) translate(-14, -14)">
+            <circle cx="14" cy="14" r="5.05" stroke="white" strokeWidth="1.9" fill="none"/>
+            <circle cx="14" cy="14" r="9.05" stroke="white" strokeOpacity="0.5" strokeWidth="1.9" fill="none"/>
+            <circle cx="14" cy="14" r="13.05" stroke="white" strokeOpacity="0.2" strokeWidth="1.9" fill="none"/>
           </g>
         </svg>
       ),
