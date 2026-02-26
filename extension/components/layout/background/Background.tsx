db31e30873c0a492c202b66647d6fb1067c3346e
Wieedze <maxime.moodz@gmail.com>
2026-02-26 08:33:57 +0100

    perf: remove unused deps and lazy-load PixelBlast to reduce bundle size (~535KB)

--- extension/components/layout/background/Background.tsx
+++ extension/components/layout/background/Background.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
-import './Background.css';
-import fondImage from './fond.png';
-import PixelBlast from './PixelBlast';
+import React, { Suspense, lazy } from "react"
+import "./Background.css"
+import fondImage from "./fond.png"
+
+const PixelBlast = lazy(() => import("./PixelBlast"))
 
 interface BackgroundProps {
   className?: string;
@@ -12,19 +13,21 @@ const Background = ({ className }: BackgroundProps) => {
     <>
       <div className={`animated-background ${className || ''}`}>
         <img className="fond" src={fondImage} alt="" />
-        <PixelBlast
-          variant="diamond"
-          pixelSize={3}
-          color="#b0b0b0"
-          patternScale={2.5}
-          patternDensity={0}
-          pixelSizeJitter={0}
-          enableRipples={false}
-          speed={2}
-          edgeFade={0.1}
-          liquid={false}
-          transparent={true}
-        />
+        <Suspense fallback={null}>
+          <PixelBlast
+            variant="diamond"
+            pixelSize={3}
+            color="#b0b0b0"
+            patternScale={2.5}
+            patternDensity={0}
+            pixelSizeJitter={0}
+            enableRipples={false}
+            speed={2}
+            edgeFade={0.1}
+            liquid={false}
+            transparent={true}
+          />
+        </Suspense>
       </div>
     </>
   );
