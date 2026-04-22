790a3b1c530f2a51dc851057c98232d1f639ea96
Wieedze <maxime.moodz@gmail.com>
2026-04-22 12:20:12 +0200

    feat(graphql): migrate address variables to arrays for multi-wallet support

--- apps/explorer/src/hooks/useLinkedWallets.ts
+++ apps/explorer/src/hooks/useLinkedWallets.ts
@@ -1,4 +1,4 @@
-cd /home/max/Project/sofia-core/core && bun run --filter @0xsofia/graphql codegenimport { useMemo } from 'react'
+import { useMemo } from 'react'
 import { usePrivy, useWallets } from '@privy-io/react-auth'
 import { getAddress, type Address } from 'viem'
 
