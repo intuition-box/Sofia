3c6d6644e3e18f6591d24b692044f123791f4147
SamuelChauche <samuel.chauche@gmail.com>
2026-05-05 16:23:12 +0200

    fix(doc): drop the 2rem auto margin on BlogLayout's ContentWrapper

--- apps/doc/src/theme/BlogLayout/index.tsx
+++ apps/doc/src/theme/BlogLayout/index.tsx
@@ -12,7 +12,7 @@ export default function BlogLayout(props: Props): ReactNode {
 
   return (
     <Layout {...layoutProps}>
-      <ContentWrapper style={{ maxWidth: '1600px', margin: '2rem auto' }}>
+      <ContentWrapper style={{ maxWidth: '1600px' }}>
         <div className="container margin-vert--lg">
           <div className="row">
             <BlogSidebar sidebar={sidebar} />
